aa946017c74185091c4c9e774b9b9c08967e470d
Oscar Guertler <oscarguertler@gmail.com>
2023-09-03 15:14:16 +0200

    add changes

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -23,7 +23,7 @@ require("bufferline").setup {
             }
         },
         diagnostics_indicator = function(count, level)
-            local icon = level:match("error") and " " or ""
+            local icon = level:match("error") and " " or " "
             return " " .. icon .. count
         end
     }
